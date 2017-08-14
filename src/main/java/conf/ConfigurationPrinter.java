2e792e04b3ccc99e9bf7a31acad9cda6672c8d0a
zhangren <zhangren@deepclue.cn>
2017-08-14 16:50:05 +0800

    add conf files & ConfigurationPrinter

---
+++ src/main/java/conf/ConfigurationPrinter.java
@@ -0,0 +1,32 @@
+package conf;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+/**
+ * Created by ggchangan on 17-8-14.
+ */
+public class ConfigurationPrinter extends Configured implements Tool {
+    static {
+        Configuration.addDefaultResource("hdfs-default.xml");
+        Configuration.addDefaultResource("hdfs-site.xml");
+        Configuration.addDefaultResource("mapred-default.xml");
+        Configuration.addDefaultResource("mapred-site.xml");
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        getConf().forEach(entry -> {
+            System.out.println(String.format("%s=%s", entry.getKey(), entry.getValue()));
+        });
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new ConfigurationPrinter(), args);
+        System.exit(exitCode);
+    }
+}
