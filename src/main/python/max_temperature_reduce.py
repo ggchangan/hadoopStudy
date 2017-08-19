b46bde22b05aa5204ec3038132c4fe8be2c2713b
zhangren <zhangren1989@gmail.com>
2017-08-19 21:48:45 +0800

    add python version MaxTemperature by Streaming

---
+++ src/main/python/max_temperature_reduce.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# 运行方法
+# cat ncdc/sample.txt | src/main/python/max_temperature_map.py |
+#  sort | src/main/python/max_temperature_reduce.py
+import sys
+
+(last_key, max_val) = (None, -sys.maxint)
+for line in sys.stdin:
+  (key, val) = line.strip().split("\t")
+  if last_key and last_key != key:
+    print "%s\t%s" % (last_key, max_val)
+    (last_key, max_val) = (key, int(val))
+  else:
+    (last_key, max_val) = (key, max(max_val, int(val)))
+
+if last_key:
+  print "%s\t%s" % (last_key, max_val)
