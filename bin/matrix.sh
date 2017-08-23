8eecf3854c858b508841ba34538854a71b247bfe
zhangren <zhangren1989@gmail.com>
2017-08-23 14:34:35 +0800

    add hprof profiles to matrix

--- bin/matrix.sh
+++ bin/matrix.sh
@@ -1,1 +1,4 @@
-hadoop jar build/libs/hadoopStudy.jar mr.matrix.MatrixDriver
+hadoop jar build/libs/hadoopStudy.jar mr.matrix.MatrixDriver -Dmapreduce.task.profile=true \
+    -Dmapreduce.task.profile.params="-agentlib:hprof=cpu=samples,heap=sites,depth=6,force=n,thread=y,verbose=n,file=%s" \
+    -Dmapreduce.task.profile.maps="0-2" -Dmapreduce.task.profile.reduces=""
+
