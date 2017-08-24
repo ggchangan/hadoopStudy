f34979a1c8fb0adcac2feb8d98808e11eb47af5e
zhangren <zhangren1989@gmail.com>
2017-08-24 14:20:49 +0800

    mv files to hdfs

--- src/main/java/hdfs/FileSystemCat.java
+++ src/main/java/hdfs/FileSystemCat.java
@@ -1,26 +1,26 @@
+package hdfs;
+
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 
 /**
  * Created by ggchangan on 17-6-22.
  */
-public class FileSystemDoubleCat {
+public class FileSystemCat {
     public static void main(String[] args) throws IOException {
+        //String uri = args[0];
         String uri = "hdfs://lc14/user/ggchangan/profile";
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(URI.create(uri), conf);
-
-        FSDataInputStream in = null;
+        InputStream in = null;
         try {
             in = fs.open(new Path(uri));
-            IOUtils.copyBytes(in, System.out, 4096, false);
-            in.seek(0);
             IOUtils.copyBytes(in, System.out, 4096, false);
         } finally {
             IOUtils.closeStream(in);
